457e4b5aadaa719f168c9c5a60e325c5b85f5aa7
dweb1 <david.p.weber@gmail.com>
2017-09-05 14:47:58 -0400

    Added routing and styling to nav bar

---
+++ client/src/components/Artist.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Artist extends Component {
+    render() {
+        return (
+            <div>
+                <h1> hello from artist</h1>
+            </div>
+        );
+    }
+}
+
+export default Artist;
